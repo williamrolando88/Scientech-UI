786dabfbafac7ec1462a40502e8795b4f3a52a7e
williamrolando88 <william.rolando88@hotmail.com>
2022-05-31 18:16:46 -0500

    Install dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="text-5xl text-red-500">
+      <span>App</span>
+    </div>
+  );
 };
 
 export default App;
