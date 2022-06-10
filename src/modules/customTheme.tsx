b01eb7acbd9739b2d7016f1be301f387aed1f24e
williamrolando88 <william.rolando88@hotmail.com>
2022-06-10 16:40:14 -0500

    Add customTheme provider

---
+++ src/modules/customTheme.tsx
@@ -0,0 +1,8 @@
+import { createTheme } from '@mui/material';
+import { blue } from '@mui/material/colors';
+
+export const scientechTheme = createTheme({
+  palette: {
+    primary: blue,
+  },
+});
