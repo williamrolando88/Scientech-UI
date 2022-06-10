97a14e313ac88cc76786c30ebcdd279fbdd10757
williamrolando88 <william.rolando88@hotmail.com>
2022-06-10 17:41:27 -0500

    Add carousel mockData

---
+++ src/modules/mockData.ts
@@ -0,0 +1,32 @@
+export const carouselData = [
+  {
+    title: 'Product 1',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores est nostrum repudiandae. Cum ullam magni quod nihil, aperiam eaque labore, laudantium, omnis eveniet dolor voluptatem. Veritatis quidem velit impedit obcaecati?',
+    target: 'product1',
+  },
+  {
+    title: 'Product 2',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores est nostrum repudiandae. Cum ullam magni quod nihil, aperiam eaque labore, laudantium, omnis eveniet dolor voluptatem. Veritatis quidem velit impedit obcaecati?',
+    target: 'product2',
+  },
+  {
+    title: 'Product 3',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores est nostrum repudiandae. Cum ullam magni quod nihil, aperiam eaque labore, laudantium, omnis eveniet dolor voluptatem. Veritatis quidem velit impedit obcaecati?',
+    target: 'product3',
+  },
+  {
+    title: 'Product 4',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores est nostrum repudiandae. Cum ullam magni quod nihil, aperiam eaque labore, laudantium, omnis eveniet dolor voluptatem. Veritatis quidem velit impedit obcaecati?',
+    target: 'product4',
+  },
+  {
+    title: 'Product 5',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores est nostrum repudiandae. Cum ullam magni quod nihil, aperiam eaque labore, laudantium, omnis eveniet dolor voluptatem. Veritatis quidem velit impedit obcaecati?',
+    target: 'product5',
+  },
+];
