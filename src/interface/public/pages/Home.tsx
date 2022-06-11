1e567f7f490c8e613d36b0ec4cf9b22ac64dbe1d
williamrolando88 <william.rolando88@hotmail.com>
2022-06-10 20:51:41 -0500

    Add project cards

--- src/interface/public/pages/Home.tsx
+++ src/interface/public/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Typography } from '@mui/material';
 import ProductsCarousel from '../components/ProductsCarousel';
+import MarqueComponent from '../components/MarqueComponent';
+import { Card, CardContent, CardHeader, CardMedia } from '@mui/material';
 
 const Home = () => {
   return (
@@ -20,6 +21,51 @@ const Home = () => {
           inventore soluta eligendi ea aspernatur deleniti.
         </p>
       </section>
+
+      <section className="flex flex-col md:flex-row gap-6 justify-between my-10 w-4/5 m-auto">
+        <div className="flex-1">
+          <Card variant="outlined">
+            <CardMedia
+              component="img"
+              height="200"
+              image="https://picsum.photos/300/200?random=1"
+              alt="random picture"
+            />
+            <CardHeader title="Insumos industriales" />
+            <CardContent>Compramos muchas cosas</CardContent>
+          </Card>
+        </div>
+        <div className="flex-1">
+          <Card variant="elevation">
+            <CardMedia
+              component="img"
+              height="200"
+              image="https://picsum.photos/300/200?random=2"
+              alt="random picture"
+            />
+            <CardHeader title="Maquinaria y repuestos" />
+            <CardContent>
+              Tambien compramos maquinas... muy grandes y peques
+            </CardContent>
+          </Card>
+        </div>
+        <div className="flex-1">
+          <Card>
+            <CardMedia
+              component="img"
+              height="200"
+              image="https://picsum.photos/300/200?random=3"
+              alt="random picture"
+            />
+            <CardHeader title="Automatizacion industrial" />
+            <CardContent>
+              Se dise~a y se instala esas cositas electronicas
+            </CardContent>
+          </Card>
+        </div>
+      </section>
+
+      <MarqueComponent />
     </>
   );
 };
