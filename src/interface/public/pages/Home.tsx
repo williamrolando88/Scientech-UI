d09fac671fd84b26098ab10898c075a50e8435ef
williamrolando88 <william.rolando88@hotmail.com>
2022-06-13 15:23:21 -0500

    Add workcard as reusable component

--- src/interface/public/pages/Home.tsx
+++ src/interface/public/pages/Home.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ProductsCarousel from '../components/ProductsCarousel';
 import MarqueComponent from '../components/MarqueComponent';
 import { Card, CardContent, CardHeader, CardMedia } from '@mui/material';
+import WorkCard from '../components/WorkCard';
+import { servicios } from '../../../modules/StaticData';
 
 const Home = () => {
   return (
@@ -24,47 +26,10 @@ const Home = () => {
         </p>
       </section>
 
-      <section className="flex flex-col md:flex-row gap-6 justify-between my-10 w-4/5 m-auto">
-        <div className="flex-1">
-          <Card variant="outlined">
-            <CardMedia
-              component="img"
-              height="200"
-              image="https://picsum.photos/300/200?random=1"
-              alt="random picture"
-            />
-            <CardHeader title="Insumos industriales" />
-            <CardContent>Compramos muchas cosas</CardContent>
-          </Card>
-        </div>
-        <div className="flex-1">
-          <Card variant="elevation">
-            <CardMedia
-              component="img"
-              height="200"
-              image="https://picsum.photos/300/200?random=2"
-              alt="random picture"
-            />
-            <CardHeader title="Maquinaria y repuestos" />
-            <CardContent>
-              Tambien compramos maquinas... muy grandes y peques
-            </CardContent>
-          </Card>
-        </div>
-        <div className="flex-1">
-          <Card>
-            <CardMedia
-              component="img"
-              height="200"
-              image="https://picsum.photos/300/200?random=3"
-              alt="random picture"
-            />
-            <CardHeader title="Automatizacion industrial" />
-            <CardContent>
-              Se dise~a y se instala esas cositas electronicas
-            </CardContent>
-          </Card>
-        </div>
+      <section className="grid grid-cols-1 md:grid-cols-3 gap-6 justify-between my-10 w-4/5 m-auto">
+        {servicios.map((service, i) => (
+          <WorkCard content={service} key={i} />
+        ))}
       </section>
 
       <MarqueComponent />
