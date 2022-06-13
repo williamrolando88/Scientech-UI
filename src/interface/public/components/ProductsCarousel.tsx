b3adb2bc311bb38d5000e184aeed1742f4452bb7
williamrolando88 <william.rolando88@hotmail.com>
2022-06-13 14:54:01 -0500

    Add dynamic pictures to carousel

--- src/interface/public/components/ProductsCarousel.tsx
+++ src/interface/public/components/ProductsCarousel.tsx
@@ -9,12 +9,13 @@ interface SlideProps {
     title: string;
     description: string;
     target: string;
+    picture: string;
   };
 }
 
 const ProductsCarousel = () => {
   return (
-    <Carousel>
+    <Carousel swipe animation="slide">
       {carouselData.map((product, i) => (
         <Slide key={i} product={product} />
       ))}
@@ -24,11 +25,13 @@ const ProductsCarousel = () => {
 
 export default ProductsCarousel;
 
-const Slide = ({ product: { title, description, target } }: SlideProps) => {
+const Slide = ({
+  product: { title, description, target, picture },
+}: SlideProps) => {
   return (
     <Paper elevation={0}>
-      <div className="grid grid-cols-2 h-80">
-        <img src="#" alt="product picture" />
+      <div className="grid grid-cols-2 h-80 content-center">
+        <img className="m-auto" src={picture} alt="product picture" />
         <div>
           <h3>{title}</h3>
           <p>{description}</p>
