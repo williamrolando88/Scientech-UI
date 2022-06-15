9042128fe8ce7b04bd80f00bd8448d476825bda5
williamrolando88 <william.rolando88@hotmail.com>
2022-06-15 16:31:18 -0500

    Add contact page skeleton

--- src/interface/public/pages/Contact.tsx
+++ src/interface/public/pages/Contact.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <section className="flex h-60">
+        <img
+          className="object-cover w-screen"
+          src="https://picsum.photos/1500/400?random=30"
+          alt=""
+        />
+      </section>
+
+      <section>
+        <h3>Informacion</h3>
+        <div className="grid grid-cols-1">
+          <div>hola</div>
+          <div>direccion</div>
+          <div>telefonos</div>
+        </div>
+      </section>
+
+      <section className="h-60 w-4/5 mx-auto bg-gray-500">
+        Aqui va el mapa
+      </section>
+
+      <section>formulario de contacto</section>
+    </>
+  );
 };
 
 export default Contact;
