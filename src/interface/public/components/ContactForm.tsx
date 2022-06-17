3384960f7365d4ddf42e27e65a302d1ee0180881
williamrolando88 <william.rolando88@hotmail.com>
2022-06-17 12:56:45 -0500

    Add basic style to contactForm

--- src/interface/public/components/ContactForm.tsx
+++ src/interface/public/components/ContactForm.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
+import { Button } from '@mui/material';
 
 const ContactForm = () => {
   const [state, handleSubmit] = useForm('myyovajy');
@@ -9,69 +10,88 @@ const ContactForm = () => {
       {state.succeeded ? (
         'Gracias por contactarnos, en breve un representante se comunicara contigo'
       ) : (
-        <form action="submit" onSubmit={handleSubmit}>
-          <h3>
-            Dejanos llegar a ti, deja tus datos y nos contactaremos contigo lo
-            mas pronto posible
-          </h3>
+        <form
+          className="flex flex-col items-center gap-10 md:flex-row"
+          action="submit"
+          onSubmit={handleSubmit}
+        >
+          <div className="flex-1">
+            <h3 className="py-10 text-center text-2xl font-bold md:text-left">
+              Déjanos llegar a tí, llena tus datos y nos contactaremos contigo
+              lo más pronto posible
+            </h3>
+          </div>
 
-          <label htmlFor="nombre">
-            Nombre y Apellido:<i>*</i>
-          </label>
-          <br />
-          <input id="nombre" name="nombre" type="text" required />
-          <ValidationError
-            prefix="Nombre"
-            field="nombre"
-            errors={state.errors}
-          />
-          <br />
+          <div className="flex w-full flex-1 flex-col items-start gap-1 ">
+            <label htmlFor="nombre">
+              Nombre y Apellido:<i className="font-bold text-red-500">*</i>
+            </label>
+            <input
+              className="mb-2 w-full rounded-sm border px-2 py-1 shadow-sm focus:ring-2 md:w-1/2"
+              id="nombre"
+              name="nombre"
+              type="text"
+              required
+            />
+            <ValidationError
+              prefix="Nombre"
+              field="nombre"
+              errors={state.errors}
+            />
 
-          <label htmlFor="correo">
-            Correo electronico:
-            <i>*</i>
-          </label>
-          <br />
-          <input id="correo" name="correo" type="email" required />
-          <ValidationError
-            prefix="Correo"
-            field="correo"
-            errors={state.errors}
-          />
-          <br />
+            <label htmlFor="correo">
+              Correo electronico:
+              <i className="font-bold text-red-500">*</i>
+            </label>
+            <input
+              className="mb-2 w-full rounded-sm border px-2 py-1 shadow-sm focus:ring-2 md:w-1/2"
+              id="correo"
+              name="correo"
+              type="email"
+              required
+            />
+            <ValidationError
+              prefix="Correo"
+              field="correo"
+              errors={state.errors}
+            />
 
-          <label htmlFor="telefono">
-            Teléfono:
-            <i>*</i>
-          </label>
-          <br />
-          <input
-            id="telefono"
-            name="telefono"
-            type="tel"
-            pattern="[0-9]{9,10}"
-            required
-          />
-          <ValidationError
-            prefix="Telefono"
-            field="telefono"
-            errors={state.errors}
-          />
-          <br />
+            <label htmlFor="telefono">
+              Teléfono:
+              <i className="font-bold text-red-500">*</i>
+            </label>
+            <input
+              className="mb-2 w-full rounded-sm border px-2 py-1 shadow-sm focus:ring-2 md:w-1/2"
+              id="telefono"
+              name="telefono"
+              type="tel"
+              pattern="[0-9]{9,10}"
+              required
+            />
+            <ValidationError
+              prefix="Telefono"
+              field="telefono"
+              errors={state.errors}
+            />
 
-          <label htmlFor="mensaje">
-            Tienes alguna pregunta, comentario o sugerencia?
-          </label>
-          <br />
-          <textarea name="mensaje" id="mensaje"></textarea>
-          <br />
+            <label htmlFor="mensaje">
+              Tienes alguna pregunta, comentario o sugerencia?
+            </label>
+            <textarea
+              className=" h-32 w-full rounded-sm border px-2 py-1 shadow-sm focus:ring-2 md:w-1/2"
+              name="mensaje"
+              id="mensaje"
+            ></textarea>
+            <i className="mb-2 text-sm">* Información requerida</i>
 
-          <button type="submit" disabled={state.submitting}>
-            Enviar
-          </button>
-          <br />
-
-          <i>* Información requerida</i>
+            <Button
+              variant="outlined"
+              type="submit"
+              disabled={state.submitting}
+            >
+              Enviar
+            </Button>
+          </div>
         </form>
       )}
     </div>
