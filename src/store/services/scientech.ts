06338066b76649f3a08bf533206fa36caed0df44
williamrolando88 <william.rolando88@hotmail.com>
2022-06-28 10:14:44 -0500

    Delete commented code

--- src/store/services/scientech.ts
+++ src/store/services/scientech.ts
@@ -17,15 +17,4 @@ export const scientechApi = createApi({
   }),
 });
 
-/* 
-export const pokemonApi = createApi({
-  reducerPath: 'pokemonApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
-  endpoints: (builder) => ({
-    getPokemonByName: builder.query<Pokemon, string>({
-      query: (name) => `pokemon/${name}`,
-    }),
-  }),
-})
-
-*/
+export const { useLoginMutation } = scientechApi;
