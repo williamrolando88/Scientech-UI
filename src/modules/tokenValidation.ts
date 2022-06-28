f5222c709d67b7231e77650f937de0969d83acdb
williamrolando88 <william.rolando88@hotmail.com>
2022-06-28 16:17:53 -0500

    Add function to check for not-Expired token

---
+++ src/modules/tokenValidation.ts
@@ -0,0 +1,14 @@
+import jwtDecode from 'jwt-decode';
+
+interface DecodedToken {
+  user_id: string;
+  role: string;
+  exp: number;
+}
+
+export const isFreshToken = (token: string): boolean => {
+  const { exp } = jwtDecode(token) as DecodedToken;
+  const now = new Date().getTime();
+  if (exp * 1000 > now) return true;
+  return false;
+};
