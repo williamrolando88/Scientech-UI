ce46ae73aa1e6c0f0154047860a7b015d013fe2d
williamrolando88 <william.rolando88@hotmail.com>
2022-07-09 13:24:40 -0500

    Add Token class

---
+++ src/modules/tokenClass.ts
@@ -0,0 +1,64 @@
+import jwtDecode from 'jwt-decode';
+
+type DecodedToken = {
+  user_id: string;
+  user_data: {
+    name: string;
+    email: string;
+  };
+  role: string;
+  exp: number;
+};
+
+type UserData = {
+  name: string;
+  email: string;
+  role: string;
+};
+
+class Token {
+  #token: string;
+  constructor() {
+    this.#token = '';
+  }
+
+  tokenExists = () => {
+    if (sessionStorage.getItem('User')) return true;
+    else return false;
+  };
+
+  getToken = () => {
+    return sessionStorage.getItem('User') as string;
+  };
+
+  isValid = (token: string) => {
+    const { exp } = jwtDecode(token) as DecodedToken;
+    const now = new Date().getTime();
+    if (exp * 1000 > now) return true;
+    return false;
+  };
+
+  getUserInfo = (token: string): UserData => {
+    const { user_data, role } = jwtDecode(token) as DecodedToken;
+    return { ...user_data, role };
+  };
+
+  loadToken = () => {
+    if (this.tokenExists()) {
+      this.#token = this.getToken();
+      if (this.isValid(this.#token)) {
+        return this.getUserInfo(this.#token);
+      } else {
+        sessionStorage.removeItem('User');
+      }
+    }
+
+    return null;
+  };
+
+  static setSessionToken = (token: string) => {
+    sessionStorage.setItem('User', JSON.stringify(token));
+  };
+}
+
+export default Token;
