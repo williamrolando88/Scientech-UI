9f4ada62e297435265bc4f5f4779308d87120542
williamrolando88 <william.rolando88@hotmail.com>
2022-07-09 12:11:28 -0500

    Add navbar

--- src/interface/private/components/DashboardNavbar.tsx
+++ src/interface/private/components/DashboardNavbar.tsx
@@ -1,5 +1,25 @@
+import { NavLink } from 'react-router-dom';
+
+interface DashboardNavLink {
+  name: string;
+  to: string;
+}
+
+const dashboardLinks: DashboardNavLink[] = [
+  { name: 'Calculadora', to: '/dashboard/calculadora' },
+  { name: 'Facturas', to: '/dashboard/facturas' },
+];
+
 const DashboardNavbar = () => {
-  return <div>DashboardNavbar</div>;
+  return (
+    <>
+      {dashboardLinks.map((link, i) => (
+        <NavLink key={i} to={link.to}>
+          {link.name}
+        </NavLink>
+      ))}
+    </>
+  );
 };
 
 export default DashboardNavbar;
