b30e936b1cc22444781895220e09c3c91c748650
williamrolando88 <william.rolando88@hotmail.com>
2022-07-13 11:47:14 -0500

    Add token validation on app mounting

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useLayoutEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from './interface/LoginPage';
 import Private from './interface/private/Private';
@@ -8,17 +8,26 @@ import Home from './interface/public/pages/Home';
 import Public from './interface/public/Public';
 import ImportCalculator from './interface/private/apps/ImportCalculator';
 import InvoiceReader from './interface/private/apps/InvoiceReader';
+import Token from './modules/tokenClass';
+import { useDispatch } from 'react-redux';
+import { setUser } from './store/reducers/user';
 
 const App = () => {
+  const session = new Token();
+  const loggedStatus = session.isValid();
+  const userInfo = session.loadToken();
+  const dispatch = useDispatch();
+  dispatch(setUser(userInfo));
+
   return (
     <>
       <Routes>
-        <Route path="login" element={<LoginPage />} />
+        <Route path="login" element={<LoginPage isLogged={loggedStatus} />} />
         <Route path="/" element={<Public />}>
           <Route index element={<Home />} />
           <Route path="contacto" element={<Contact />} />
         </Route>
-        <Route path="dashboard" element={<Private />}>
+        <Route path="dashboard" element={<Private isLogged={loggedStatus} />}>
           <Route index element={<PrivateIndex />} />
           <Route path="calculadora" element={<ImportCalculator />} />
           <Route path="facturas" element={<InvoiceReader />} />
