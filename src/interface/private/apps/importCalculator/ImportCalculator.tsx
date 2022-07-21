193b64c7a778b4ac827c23a62061bbd4ee7b2ddd
williamrolando88 <william.rolando88@hotmail.com>
2022-07-21 13:53:21 -0500

    Add calculator skeleton

--- src/interface/private/apps/importCalculator/ImportCalculator.tsx
+++ src/interface/private/apps/importCalculator/ImportCalculator.tsx
@@ -1,5 +1,22 @@
+import ArticlesContainer from './components/ArticlesContainer';
+
 const ImportCalculator = () => {
-  return <section>ImportCalculator</section>;
+  return (
+    <section>
+      <div>
+        <button>Nuevo</button>
+        <button>Guardar</button>
+        <button>Recuperar</button>
+        <button>Limpiar</button>
+      </div>
+
+      <ArticlesContainer />
+
+      <div>aqui vienen los costos fijos</div>
+
+      <div>aqui viene el reporte</div>
+    </section>
+  );
 };
 
 export default ImportCalculator;
